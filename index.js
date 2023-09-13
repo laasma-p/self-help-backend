e2337baf33cb700279649eea17af0d1aebe19a9d
Lasma Poksane <laasmapoksane@gmail.com>
2023-09-13 10:45:02 +0200

    add sign-up endpoint to register users

--- index.js
+++ index.js
@@ -1,13 +1,38 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const cors = require("cors");
+const bodyParser = require("body-parser");
+const bcrypt = require("bcrypt");
+const db = require("./db");
 
 const app = express();
 dotenv.config();
 
 app.use(cors());
 
+app.use(bodyParser.json());
+
 const PORT = process.env.PORT;
+
+app.post("/sign-up", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    const saltRounds = 10;
+    const salt = await bcrypt.genSalt(saltRounds);
+
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const result = await db("users").insert({
+      email,
+      password: hashedPassword,
+    });
+
+    return res.status(201).json({ message: "User registered successfully." });
+  } catch (error) {
+    return res.status(500).json({ message: "Internal server error." });
+  }
+});
 
 app.listen(PORT, () => {
   console.log(`Server is listening for requests on port ${PORT}`);
