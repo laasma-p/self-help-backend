ab15078cf0f8b9cc84e30ca0e138604bab69af65
Lasma Poksane <laasmapoksane@gmail.com>
2023-09-13 10:45:47 +0200

    add login endpoint to log in users

--- index.js
+++ index.js
@@ -34,6 +34,28 @@ app.post("/sign-up", async (req, res) => {
   }
 });
 
+app.post("/login", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    const user = await db("users").where({ email }).first();
+
+    if (!user) {
+      return res.status(401).json({ message: "Invalid email or password." });
+    }
+
+    const passwordMatch = await bcrypt.compare(password, user.password);
+
+    if (!passwordMatch) {
+      return res.status(401).json({ message: "Invalid email or password." });
+    }
+
+    return res.status(200).json({ message: "Login successful" });
+  } catch (error) {
+    return res.status(500).json({ message: "Internal server error." });
+  }
+});
+
 app.listen(PORT, () => {
   console.log(`Server is listening for requests on port ${PORT}`);
 });
